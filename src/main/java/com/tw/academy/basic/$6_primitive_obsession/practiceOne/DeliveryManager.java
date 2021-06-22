9dff00f970a02f80390b8f84b42d7528f14cb26d
eddy guo <guoed3@oocl.com>
2021-06-22 16:03:27 +0800

    [refact] primitive obsession: remove unused method

--- src/main/java/com/tw/academy/basic/$6_primitive_obsession/practiceOne/DeliveryManager.java
+++ src/main/java/com/tw/academy/basic/$6_primitive_obsession/practiceOne/DeliveryManager.java
@@ -23,12 +23,4 @@ public class DeliveryManager {
         }
         return DeliverCenter.FOREIGN;
     }
-
-    private String getCity(String address) {
-        return address.substring(address.indexOf("省") + 1, address.indexOf("市"));
-    }
-
-    private String getProvince(String address) {
-        return address.substring(0, address.indexOf("省"));
-    }
 }
