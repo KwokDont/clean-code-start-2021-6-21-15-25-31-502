30a17fea96d8bedbbb51534ce3e495f4f881e3f8
eddy guo <guoed3@oocl.com>
2021-06-22 10:52:48 +0800

    [refact] magic number

--- src/main/java/com/tw/academy/basic/$2_magic_number/FareCalculation.java
+++ src/main/java/com/tw/academy/basic/$2_magic_number/FareCalculation.java
@@ -2,11 +2,16 @@ package com.tw.academy.basic.$2_magic_number;
 
 public class FareCalculation {
 
+    public static final Integer EIGHT_DISC_FROM = 100;
+    public static final Integer EIGHT_DISC_TO = 150;
+    public static final Integer FIVE_DISC_FROM = 150;
+    public static final Integer FIVE_DISC_TO = 400;
+
     public double calculate(double originalPrice, double cumulativeExpensesThisMonth) {
-        if (cumulativeExpensesThisMonth >= 100 && cumulativeExpensesThisMonth < 150) {
+        if (cumulativeExpensesThisMonth >= EIGHT_DISC_FROM && cumulativeExpensesThisMonth < EIGHT_DISC_TO) {
             return originalPrice * 0.8;
         }
-        if (cumulativeExpensesThisMonth >= 150 && cumulativeExpensesThisMonth < 400) {
+        if (cumulativeExpensesThisMonth >= FIVE_DISC_FROM && cumulativeExpensesThisMonth < FIVE_DISC_TO) {
             return originalPrice * 0.5;
         }
         return originalPrice;
