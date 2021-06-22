f38e442976d8e4a84ed9578332811fad1f302323
eddy guo <guoed3@oocl.com>
2021-06-22 10:59:42 +0800

    [refact] feature envy

--- src/main/java/com/tw/academy/basic/$3_feature_envy/practiceOne/Phone.java
+++ src/main/java/com/tw/academy/basic/$3_feature_envy/practiceOne/Phone.java
@@ -2,16 +2,31 @@ package com.tw.academy.basic.$3_feature_envy.practiceOne;
 
 public class Phone {
     private final String unformattedNumber;
+    private String areaCode;
+    private String prefix;
+    private String number;
+
     public Phone(String unformattedNumber) {
         this.unformattedNumber = unformattedNumber;
+        this.areaCode = unformattedNumber.substring(0,3);
+        this.prefix = unformattedNumber.substring(3,6);
+        this.number = unformattedNumber.substring(6,10);
     }
+
+    public String getMobilePhoneNumber() {
+        return "(" + this.areaCode + ")" +
+                this.prefix + "-" + this.number;
+    }
+
     public String getAreaCode() {
-        return unformattedNumber.substring(0,3);
+        return areaCode;
     }
+
     public String getPrefix() {
-        return unformattedNumber.substring(3,6);
+        return prefix;
     }
+
     public String getNumber() {
-        return unformattedNumber.substring(6,10);
+        return number;
     }
 }
