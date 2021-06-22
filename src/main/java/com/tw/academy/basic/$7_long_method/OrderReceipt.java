6a3639c4d6a2b91386785b20750a2ad8087ba9b9
eddy guo <guoed3@oocl.com>
2021-06-23 00:48:27 +0800

    [refactor] remove comment

--- src/main/java/com/tw/academy/basic/$7_long_method/OrderReceipt.java
+++ src/main/java/com/tw/academy/basic/$7_long_method/OrderReceipt.java
@@ -28,23 +28,18 @@ public class OrderReceipt {
 
         output.append(order.getCustomerInfo());
 
-        // prints lineItems
         double totalSalesTax = 0d;
         double totalAmount = 0d;
         for (LineItem lineItem : order.getLineItems()) {
             output.append(lineItem.genLineItemPrintInfo());
 
-            // calculate sales tax @ rate of 10%
             totalSalesTax += lineItem.getItemSalesTax();
 
-            // calculate total amount of lineItem = price * quantity + 10 % sales tax
             totalAmount += lineItem.getTotalAmountWithTax();
         }
 
-        // prints the state tax
         output.append(SALE_TAX).append(TAB).append(totalSalesTax);
 
-        // print total amount
         output.append(TOTAL_AMOUNT).append(TAB).append(totalAmount);
         return output.toString();
     }
